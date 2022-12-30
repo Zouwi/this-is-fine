600e25bba31ce022fa51d13f28617397cbbc29b9
Zouwi <119571285+Zouwi@users.noreply.github.com>
2022-12-30 20:10:12 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,113 @@
+window.onscroll = function () { bestscroll() }
+
+function bestscroll() {
+    if (document.documentElement.scrollTop > 550) {
+        document.querySelector('body').style.backgroundColor = '#1A2434';
+        document.querySelector('.grostitre3').style.color = '#CDA7D7';
+        document.querySelector('.grostitre5').style.color = '#FFFFFF';
+        // la terre svg 
+        document.querySelector('#Vector').style.fill = '#355575';
+        document.querySelector('#Vector_2').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_2').style.fill = '#82628A';
+        document.querySelector('#Vector_4').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_4').style.fill = '#8FC9FF';
+        document.querySelector('#Vector_6').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_7').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_8').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_9').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_9').style.fill = '#FCFCEC';
+        document.querySelector('#Vector_10').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_11').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_11').style.fill = '#8FC9FF';
+        document.querySelector('#Vector_12').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_12').style.fill = '#8FC9FF';
+        document.querySelector('#Vector_13').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_13').style.fill = '#FCFCEC';
+        document.querySelector('#Vector_14').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_14').style.fill = '#82628A';
+        document.querySelector('#Vector_15').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_15').style.fill = '#82628A';
+        document.querySelector('#Vector_16').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_16').style.fill = '#82628A';
+        document.querySelector('#Vector_17').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_17').style.fill = '#82628A';
+        document.querySelector('#Vector_18').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_18').style.fill = '#82628A';
+        document.querySelector('#Vector_19').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_19').style.fill = '#82628A';
+        document.querySelector('#Vector_20').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_20').style.fill = '#8FC9FF';
+        document.querySelector('#Vector_21').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_22').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_23').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_24').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_25').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_26').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_27').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_28').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_29').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_30').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_31').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_32').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_33').style.stroke = '#CDA7D7';
+        document.querySelector('#Vector_34').style.stroke = '#CDA7D7';
+    }
+    if (document.documentElement.scrollTop < 550) {
+        document.querySelector('body').style.backgroundColor = '#FCFCEC';
+        document.querySelector('.grostitre3').style.color = '#C15535';
+        document.querySelector('.grostitre5').style.color = '#355575';
+        // la terre svg 
+        document.querySelector('#Vector').style.fill = '#D9E7E2';
+        document.querySelector('#Vector_2').style.stroke = '#C15535';
+        document.querySelector('#Vector_2').style.fill = '#FCFBEE';
+        document.querySelector('#Vector_4').style.stroke = '#C15535';
+        document.querySelector('#Vector_4').style.fill = '#FCFBEE';
+        document.querySelector('#Vector_6').style.stroke = '#C15535';
+        document.querySelector('#Vector_7').style.stroke = '#C15535';
+        document.querySelector('#Vector_8').style.stroke = '#C15535';
+        document.querySelector('#Vector_9').style.stroke = '#C15535';
+        document.querySelector('#Vector_9').style.fill = '#E3CFC6';
+        document.querySelector('#Vector_10').style.stroke = '#C15535';
+        document.querySelector('#Vector_11').style.stroke = '#C15535';
+        document.querySelector('#Vector_11').style.fill = '#FCFBEE';
+        document.querySelector('#Vector_12').style.stroke = '#C15535';
+        document.querySelector('#Vector_12').style.fill = '#FCFBEE';
+        document.querySelector('#Vector_13').style.stroke = '#C15535';
+        document.querySelector('#Vector_13').style.fill = '#E3CFC6';
+        document.querySelector('#Vector_14').style.stroke = '#C15535';
+        document.querySelector('#Vector_14').style.fill = '#C292A4';
+        document.querySelector('#Vector_15').style.stroke = '#C15535';
+        document.querySelector('#Vector_15').style.fill = '#C292A4';
+        document.querySelector('#Vector_16').style.stroke = '#C15535';
+        document.querySelector('#Vector_16').style.fill = '#C292A4';
+        document.querySelector('#Vector_17').style.stroke = '#C15535';
+        document.querySelector('#Vector_17').style.fill = '#C292A4';
+        document.querySelector('#Vector_18').style.stroke = '#C15535';
+        document.querySelector('#Vector_18').style.fill = '#C292A4';
+        document.querySelector('#Vector_19').style.stroke = '#C15535';
+        document.querySelector('#Vector_19').style.fill = '#C292A4';
+        document.querySelector('#Vector_20').style.stroke = '#C15535';
+        document.querySelector('#Vector_20').style.fill = '#FCFBEE';
+        document.querySelector('#Vector_21').style.stroke = '#C15535';
+        document.querySelector('#Vector_22').style.stroke = '#C15535';
+        document.querySelector('#Vector_23').style.stroke = '#C15535';
+        document.querySelector('#Vector_24').style.stroke = '#C15535';
+        document.querySelector('#Vector_25').style.stroke = '#C15535';
+        document.querySelector('#Vector_26').style.stroke = '#C15535';
+        document.querySelector('#Vector_27').style.stroke = '#C15535';
+        document.querySelector('#Vector_28').style.stroke = '#C15535';
+        document.querySelector('#Vector_29').style.stroke = '#C15535';
+        document.querySelector('#Vector_30').style.stroke = '#C15535';
+        document.querySelector('#Vector_31').style.stroke = '#C15535';
+        document.querySelector('#Vector_32').style.stroke = '#C15535';
+        document.querySelector('#Vector_33').style.stroke = '#C15535';
+        document.querySelector('#Vector_34').style.stroke = '#C15535';
+    }
+    if (document.documentElement.scrollTop > 5300) {
+        document.querySelector('body').style.backgroundColor = '#FCFCEC';
+        document.querySelector('titre').style.color = '#355575';
+    }
+    if (document.documentElement.scrollTop > 1200) {
+        document.querySelector('.traitrond').style.animation = 'traitrond 2s linear';
+    }
+}
