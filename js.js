77b7cf06225e5fade2d756bb83d3c787f369e0fa
Zouwi <119571285+Zouwi@users.noreply.github.com>
2022-12-31 18:10:40 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -1,3 +1,4 @@
+// Les animations avec le scroll 
 window.onscroll = function () { bestscroll() }
 
 function bestscroll() {
@@ -108,6 +109,91 @@ function bestscroll() {
         document.querySelector('titre').style.color = '#355575';
     }
     if (document.documentElement.scrollTop > 1200) {
-        document.querySelector('.traitrond').style.animation = 'traitrond 2s linear';
+        document.querySelector('.traitrond').style.animation = 'traitrond 2s';
+        document.querySelector('.rondrond').style.animation = 'rondrond 2s';
     }
+    if (document.documentElement.scrollTop > 1650) {
+        document.querySelector('.ligne').style.animation = 'traitfact 2s ';
+        document.querySelector('.rondfact').style.animation = 'rondfact 2s ';
+        document.querySelector('.ligne2').style.animation = 'traitfact 2s ';
+        document.querySelector('.rondfact2').style.animation = 'rondfact 2s ';
+        document.querySelector('.ligne3').style.animation = 'traitfact 2s ';
+        document.querySelector('.rondfact3').style.animation = 'rondfact 2s ';
+        document.querySelector('.ligne4').style.animation = 'traitfact 2s ';
+        document.querySelector('.rondfact4').style.animation = 'rondfact 2s ';
+        document.querySelector('.ligne5').style.animation = 'traitfact 2s ';
+        document.querySelector('.rondfact5').style.animation = 'rondfact 2s ';
+    }
+    if (document.documentElement.scrollTop > 1650) {
+        document.querySelector('.rondchiffre').style.animation = 'rondchiffre 2s';
+        document.querySelector('.traitchiffre').style.animation = 'traitchiffre 2s';
+    }
+}
+
+// Les intéractions
+// Les plus vulnerables
+
+document.querySelector('.fauche').addEventListener("click", vulnerablefauche);
+
+function vulnerablefauche() {
+    document.querySelector('.fermer').classList.toggle('ouvert');
+}
+
+document.querySelector('.couleur').addEventListener("click", vulnerablecouleur);
+
+function vulnerablecouleur() {
+    document.querySelector("body > div.vulnerable > div.popup2.fermer").classList.toggle('ouvert');
+}
+
+document.querySelector('.lgbt').addEventListener("click", vulnerablelgbt);
+
+function vulnerablelgbt() {
+    document.querySelector("body > div.vulnerable > div.popup3.fermer").classList.toggle('ouvert');
+}
+
+document.querySelector('.femme').addEventListener("click", vulnerablefemme);
+
+function vulnerablefemme() {
+    document.querySelector("body > div.vulnerable > div.popup4.fermer").classList.toggle('ouvert');
+}
+
+document.querySelector('.enfant').addEventListener("click", vulnerableenfant);
+
+function vulnerableenfant() {
+    document.querySelector("body > div.vulnerable > div.popup5.fermer").classList.toggle('ouvert');
+}
+
+document.querySelector('.fauche').addEventListener("click", changecouleur);
+
+function changecouleur() {
+    document.querySelector(".fill11").classList.toggle('pareil');
+    document.querySelector(".fill12").classList.toggle('pareil');
+}
+
+document.querySelector('.couleur').addEventListener("click", changecouleur2);
+
+function changecouleur2() {
+    document.querySelector(".fill21").classList.toggle('pareil');
+    document.querySelector(".fill22").classList.toggle('pareil');
+}
+
+document.querySelector('.lgbt').addEventListener("click", changecouleur3);
+
+function changecouleur3() {
+    document.querySelector(".fill31").classList.toggle('pareil');
+    document.querySelector(".fill32").classList.toggle('pareil');
+}
+
+document.querySelector('.femme').addEventListener("click", changecouleur4);
+
+function changecouleur4() {
+    document.querySelector(".fill41").classList.toggle('pareil');
+    document.querySelector(".fill42").classList.toggle('pareil');
+}
+
+document.querySelector('.enfant').addEventListener("click", changecouleur5);
+
+function changecouleur5() {
+    document.querySelector(".fill51").classList.toggle('pareil');
+    document.querySelector(".fill52").classList.toggle('pareil');
 }
